98e0ae19c68d9ded02b0c5f0a7f8802fa3c57821
ganeshkdeveloper <ganeshkdeveloper2025@gmail.com>
2026-06-18 12:33:03 +0530

    Day 1 - JDBC Connection and Insert Operation

---
+++ src/Main.java
@@ -0,0 +1,23 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+
+public class Main {
+    public static void main(String[] args) {
+        try {
+            Connection con = DriverManager.getConnection(
+                "jdbc:mysql://localhost:3306/schooldb",
+                "root",
+                "root123"
+            );
+
+            Statement st=con.createStatement();
+            st.executeUpdate( "INSERT INTO students VALUES(101,'Ganesh')");
+            System.out.println("Data Inserted Successfully!");
+
+            con.close();
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+}
