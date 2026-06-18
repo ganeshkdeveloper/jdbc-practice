bb51474c9f664b6783e038c1a6aac458e23fd421
ganeshkdeveloper <ganeshkdeveloper2025@gmail.com>
2026-06-18 13:00:44 +0530

    Day 2 - JDBC Select Operation

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,28 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.Statement;
 
+// public class Main {
+//     public static void main(String[] args) {
+//         try {
+//             Connection con = DriverManager.getConnection(
+//                 "jdbc:mysql://localhost:3306/schooldb",
+//                 "root",
+//                 "root123"
+//             );
+
+//             Statement st=con.createStatement();
+//             st.executeUpdate( "INSERT INTO students VALUES(101,'Ganesh')");
+//             System.out.println("Data Inserted Successfully!");
+
+//             con.close();
+//         } catch (Exception e) {
+//             System.out.println(e);
+//         }
+//     }
+// }
+
+import java.sql.*;
+
 public class Main {
     public static void main(String[] args) {
         try {
@@ -11,12 +33,22 @@ public class Main {
                 "root123"
             );
 
-            Statement st=con.createStatement();
-            st.executeUpdate( "INSERT INTO students VALUES(101,'Ganesh')");
-            System.out.println("Data Inserted Successfully!");
+            Statement st = con.createStatement();
+
+            ResultSet rs = st.executeQuery(
+                "SELECT * FROM students"
+            );
+
+            while(rs.next()){
+                System.out.println(
+                    rs.getInt("id") + " " +
+                    rs.getString("name")
+                );
+            }
 
             con.close();
-        } catch (Exception e) {
+
+        } catch(Exception e){
             System.out.println(e);
         }
     }
