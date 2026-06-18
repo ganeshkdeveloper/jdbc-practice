32b83dbd12e12e0af7bc1305fda4a8cdf1a3fa3e
ganeshkdeveloper <ganeshkdeveloper2025@gmail.com>
2026-06-18 14:04:49 +0530

    Day 3 - JDBC Update Operation

--- src/Main.java
+++ src/Main.java
@@ -22,7 +22,41 @@ import java.sql.Statement;
 //     }
 // }
 
-import java.sql.*;
+// import java.sql.*;
+
+// public class Main {
+//     public static void main(String[] args) {
+//         try {
+//             Connection con = DriverManager.getConnection(
+//                 "jdbc:mysql://localhost:3306/schooldb",
+//                 "root",
+//                 "root123"
+//             );
+
+//             Statement st = con.createStatement();
+
+//             ResultSet rs = st.executeQuery(
+//                 "SELECT * FROM students"
+//             );
+
+//             while(rs.next()){
+//                 System.out.println(
+//                     rs.getInt("id") + " " +
+//                     rs.getString("name")
+//                 );
+//             }
+
+//             con.close();
+
+//         } catch(Exception e){
+//             System.out.println(e);
+//         }
+//     }
+// }
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
 
 public class Main {
     public static void main(String[] args) {
@@ -35,20 +69,15 @@ public class Main {
 
             Statement st = con.createStatement();
 
-            ResultSet rs = st.executeQuery(
-                "SELECT * FROM students"
+            int rows = st.executeUpdate(
+                "UPDATE students SET name='Java' WHERE id=101"
             );
 
-            while(rs.next()){
-                System.out.println(
-                    rs.getInt("id") + " " +
-                    rs.getString("name")
-                );
-            }
+            System.out.println(rows + " row updated");
 
             con.close();
 
-        } catch(Exception e){
+        } catch (Exception e) {
             System.out.println(e);
         }
     }
